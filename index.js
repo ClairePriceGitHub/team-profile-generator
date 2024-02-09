adec50b3d5e13673cbf8d2f8ebd90e345e705cdb
Claire Price <claire.price@mail.com>
2024-02-09 23:26:01 +0000

    organise functions in file

--- index.js
+++ index.js
@@ -14,83 +14,136 @@ const render = require("./src/page-template.js");
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 const teamArr = [];
 
-const questionsManager = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Enter your name',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Enter your employee ID',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address',
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Enter your office number',
-    },
-];
+createEmployees = () => {
+    // Inquirer questions for each employee type
+    const questionsManager = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter your name',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter your employee ID',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter your office number',
+        },
+    ];
 
-const questionsTeam = [
-    {
-        type: 'checkbox',
-        name: 'teamOptions',
-        message: 'Who would you like to add?',
-        choices: ['Engineer', 'Intern', 'Finish building the team']
-    },
-]
+    const questionsTeam = [
+        {
+            type: 'checkbox',
+            name: 'teamOptions',
+            message: 'Who would you like to add?',
+            choices: ['Engineer', 'Intern', 'Finish building the team']
+        },
+    ]
 
-const questionsEngineer = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Enter name',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Enter employee ID',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter email address',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter GitHub username',
-    },
-];
+    const questionsEngineer = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter name',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter employee ID',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter email address',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter GitHub username',
+        },
+    ];
 
-const questionsIntern = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Enter name',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Enter employee ID',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter email address',
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'Enter school name',
-    },
-];
+    const questionsIntern = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter name',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter employee ID',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter email address',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Enter school name',
+        },
+    ];
+
+    // Function to allow new employee type selection
+    createTeam = () => {
+        inquirer.prompt(questionsTeam)
+        .then(data => {
+            if (data.teamOptions == 'Engineer') {
+                createEngineer();
+            } else if (data.teamOptions == 'Intern') {
+                createIntern();
+            } else {
+                // fs.writeFile(outputPath, render(team), (err) =>
+                // err ? console.log(err) : console.log('Success!')
+                // );
+            }
+        })
+    }
+
+    // Functions to create each employee by type starting with manager
+    createManager = () => {
+        inquirer.prompt(questionsManager)
+        .then(data => {
+            const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+            teamArr.push(manager);
+            console.log(teamArr);
+            createTeam();
+        })
+    }
+    createManager();
+
+    createEngineer = () => {
+        inquirer.prompt(questionsEngineer)
+        .then(data => {
+            const engineer = new Engineer(data.name, data.id, data.email, data.github);
+            teamArr.push(engineer);
+            console.log(teamArr);
+            createTeam();
+        })
+    }
+
+    createIntern = () => {
+        inquirer.prompt(questionsIntern)
+        .then(data => {
+            const intern = new Intern(data.name, data.id, data.email, data.school);
+            teamArr.push(intern);
+            console.log(teamArr);
+            createTeam();
+        })
+    }
+}
+
+createEmployees();
 
 
 
@@ -102,64 +155,3 @@ const questionsIntern = [
 //     err ? console.log(err) : console.log('Success!')
 //     );
 // })
-
-
-
-function createEmployees() {
-
-
-
-function createManager() {
-    inquirer.prompt(questionsManager)
-    .then(data => {
-        const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
-        teamArr.push(manager);
-        console.log(teamArr);
-        createTeam();
-    })
-}
-createManager();
-
-function createTeam() {
-    inquirer.prompt(questionsTeam)
-    .then(data => {
-        if (data.teamOptions == 'Engineer') {
-            createEngineer();
-        } else if (data.teamOptions == 'Intern') {
-            createIntern();
-        } else {
-            // fs.writeFile(outputPath, render(team), (err) =>
-            // err ? console.log(err) : console.log('Success!')
-            // );
-        }
-        
-    })
-}
-
-function createEngineer() {
-    inquirer.prompt(questionsEngineer)
-    .then(data => {
-        const engineer = new Engineer(data.name, data.id, data.email, data.github);
-        teamArr.push(engineer);
-        console.log(teamArr);
-        createTeam();
-    })
-}
-
-function createIntern() {
-    inquirer.prompt(questionsIntern)
-    .then(data => {
-        const intern = new Intern(data.name, data.id, data.email, data.school);
-        teamArr.push(intern);
-        console.log(teamArr);
-        createTeam();
-    })
-}
-
-
-}
-
-
-
-createEmployees();
-
