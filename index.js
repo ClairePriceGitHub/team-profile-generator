74a07a39d4edf56053dd1a2a590f1a73231b2c36
Claire Price <claire.price@mail.com>
2024-02-09 21:26:58 +0000

    add all questions

--- index.js
+++ index.js
@@ -37,6 +37,61 @@ const questionsManager = [
     },
 ];
 
+const teamOptions = [
+    {
+        type: 'input',
+        name: 'teamOptions',
+        message: 'Who would you like to add?',
+        choices: ['Engineer', 'Intern', 'Finish building the team']
+    },
+]
+
+const questionsEngineer = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter name',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter employee ID',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter email address',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter GitHub username',
+    },
+];
+
+const questionsIntern = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter name',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter employee ID',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter email address',
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Enter school name',
+    },
+];
+
 
 
 
@@ -52,27 +107,32 @@ const questionsManager = [
 
 
 
-    // function createManager() {
-    //     inquirer.prompt(questionsManager)
-    //     .then((data) => {
-    //     const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
-    //     createTeam.push(manager);
-    //     console.log(createTeam);
-    //     console.log(manager.getRole());
-        
-    //     })
-    // }
+function createManager() {
+    inquirer.prompt(questionsManager)
+    .then(data => {
+        const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+        createTeam.push(manager);
+        console.log(createTeam);
+    })
+}
 
 
-
+function createTeam() {
+    inquirer.prompt(teamOptions)
+    .then(team => {
+        fs.writeFile(outputPath, render(team), (err) =>
+        err ? console.log(err) : console.log('Success!')
+        );
+    })
+}
 
 
 
 // createManager()
 
-inquirer.prompt(questionsManager)
-.then((team) => {
-    fs.writeFile('./index.html', render(team), (err) =>
-    err ? console.log(err) : console.log('Success!')
-    );
-})
+// inquirer.prompt(questionsManager)
+// .then(team => {
+//     fs.writeFile(outputPath, render(team), (err) =>
+//     err ? console.log(err) : console.log('Success!')
+//     );
+// })
