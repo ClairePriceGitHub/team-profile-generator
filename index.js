bfe0d7c693c3fee56a8f64876491e4e8ed98d554
Claire Price <claire.price@mail.com>
2024-02-09 13:55:53 +0000

    require classes, install dependancies, Manager object created

--- index.js
+++ index.js
@@ -13,3 +13,41 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Enter your name',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'Enter your employee ID',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address',
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter your office number',
+    },
+]
+
+
+// inquirer.prompt(questions)
+// .then((data) => {
+//     fs.writeFile('./index.html', module.exports(team), (err) =>
+//     err ? console.log(err) : console.log('Success!')
+//     );
+// })
+
+
+inquirer.prompt(questions)
+.then((data) => {
+    fs.writeFile('./index.html', JSON.stringify(data, null, '\t'), (err) =>
+    err ? console.log(err) : console.log('Success!')
+    );
+})
